471a73cf03ba71705850eb36eae3224023f20d8a
harshkumar001 <sainikaimla002@gmail.com>
2023-09-12 14:26:06 +0530

    Redux and Theme setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/profilePage";
+import HomePage from "./scenes/homePage/HomePage";
+
+import LoginPage from "./scenes/loginPage/LoginPage";
+import ProfilePage from "./scenes/profilePage/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
